c2a4f4eda386c89527faba1debc445ce8333555a
p17032730 <alex.picavet@gmail.com>
2018-12-07 09:18:55 +0100

    Modification de LogoLISFactory pour mettre un cercle sur le point du "i" avec la classe Circle

--- src/main/java/image/LogoLISFactory.java
+++ src/main/java/image/LogoLISFactory.java
@@ -10,16 +10,18 @@ public class LogoLISFactory implements ImageFactory{
         Color dark = Color.rgb(35,31,32);
         Color greenblue = Color.rgb(113,208,199);
 
-        int[] xCoordinates = {60,  720, 660, 400, 0,   400, 660};
-        int[] yCoordinates = {0,   60,  140, 0,   0,   140, 0};
-        int[] widths =       {140, 140, 140, 60,  200, 60,  200};
-        int[] heights =      {280, 80,  140, 80,  340, 200, 340};
-        Color[] colors = {Color.WHITE, Color.WHITE, Color.WHITE, greenblue, dark, dark, dark};
+        int[] xCoordinates = {60,  720, 660, 0,   400, 660};
+        int[] yCoordinates = {0,   60,  140,   0,   140, 0};
+        int[] widths =       {140, 140, 140,  200, 60,  200};
+        int[] heights =      {280, 80,  140,  340, 200, 340};
+        Color[] colors = {Color.WHITE, Color.WHITE, Color.WHITE, dark, dark, dark};
 
         List<Shape> list = new ArrayList<>();
 
         for (int i = 0; i<colors.length; i++)
             list.add(new Rectangle(xCoordinates[i], yCoordinates[i], widths[i], heights[i], colors[i]));
+
+        list.add(new Circle(430, 40, 40, greenblue));
 
         return new VectorImage(list, 860, 340);
     }
