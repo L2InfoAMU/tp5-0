a1a642997e1037570aee8d314527b0f4678bb5e4
p17032730 <alex.picavet@gmail.com>
2018-12-07 09:29:46 +0100

    Documentation de la classe Rectangle

--- src/main/java/image/Rectangle.java
+++ src/main/java/image/Rectangle.java
@@ -10,6 +10,15 @@ public class Rectangle implements Shape {
     private int height;
     private Color color;
 
+
+    /**
+     * Crée un rectangle avec la position haut/gauche, sa largeur, hauteur et sa couleur
+     * @param x La position horizontale du coin haut/gauche du rectangle
+     * @param y La position verticale du coin haut/gauche du rectangle
+     * @param width La largeur du rectangle
+     * @param height La hauteur du rectangle
+     * @param color La couleur du rectangle
+     */
     public Rectangle(int x, int y, int width, int height, Color color) {
         this.x = x;
         this.y = y;
@@ -18,16 +27,32 @@ public class Rectangle implements Shape {
         this.color = color;
     }
 
+
+    /**
+     * Détermine si un point est dans le rectangle
+     * @param point Le point à vérifier
+     * @return True si le point est contenu dans le rectangle, False sinon.
+     */
     @Override
     public boolean contains(Point point) {
         return point.x >= this.x && point.x <= this.x + this.width && point.y >= this.y && point.y <= this.y + this.height;
     }
 
+
+    /**
+     * Donne la couleur du rectangle
+     * @return La couleur du rectangle
+     */
     @Override
     public Color getColor() {
         return this.color;
     }
 
+
+    /**
+     * Crée et renvoie un nouveau rectangle avec les mêmes attributs
+     * @return Une référence vers un nouvel objet Rectangle ayant les mêmes attributs
+     */
     @Override
     public Shape clone() {
         return new Rectangle(this.x, this.y, this.width, this.height, this.color);
